2407b9b9e3e97724a5ae05cd60677020ef61810f
Victor <victorsmirnov@me.com>
2020-11-20 22:09:55 +0200

    222

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,23 @@ const Header = ({ fetchMovies, fetchPopular }) => {
     }
 
     return (
-        <nav className="navbar navbar-dark bg-dark fixed-top px-5">
-            <a className="navbar-brand " href="#home" onClick={handleToHome}>Movie Search</a>
+        
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top px-5">
+            <a className="navbar-brand" href="#home" onClick={handleToHome}>Movie Search</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                    <a href="#link" className="nav-link" onClick={handleToHome}>Movies</a>
+                </li>
+                <li className="nav-item">
+                    <a href="#link" className="nav-link" onClick={handleToHome}>TV Shows</a>
+                </li>
+            </ul>
             <Search fetchMovies={fetchMovies} />
+            </div>
         </nav>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
+import 'jquery/dist/jquery.min.js';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
