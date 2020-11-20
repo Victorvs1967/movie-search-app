772d2a6f9dce270daa8ed91fd80b32b449e8b678
Victor <victorsmirnov@me.com>
2020-11-20 15:22:51 +0200

    111

--- src/Const.js
+++ src/Const.js
@@ -4,6 +4,6 @@ export const API_DETAILS_URL = 'https://api.themoviedb.org/3/movie/';
 export const API_PARAMS = '&language=ru-RU&sort_by=popularity.desc';
 export const API_KEY = 'api_key=565bffc33a642464d643ce0ca8704d54';
 
-export const DEFAULT_PLACEHOLDER_IMAGE = 'https://m.media-amazon.com/images/M/MV5BMjNkMzc2N2QtNjVlNS00ZTk5LTg0MTgtODY2MDAwNTMwZjBjXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg'
-export const API_IMG_URL = 'https://image.tmdb.org/t/p/w500'
+export const DEFAULT_PLACEHOLDER_IMAGE = 'https://m.media-amazon.com/images/M/MV5BMjNkMzc2N2QtNjVlNS00ZTk5LTg0MTgtODY2MDAwNTMwZjBjXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg';
+export const API_IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
